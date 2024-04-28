75a5cdb6284ead5d4c2fe2925a9363f83ff36d26
Anushka Srivastava <srivastavarishi29@gmail.com>
2024-04-28 18:44:11 +0530

    Header Hero Section Completed Img left

---
+++ components/Hero.jsx
@@ -0,0 +1,48 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { Download, Send } from "lucide-react";
+import {
+  RiBriefcase4Fill,
+  RiTeamFill,
+  RiTodoFill,
+  RiArrowDownSLine,
+} from "react-icons/ri";
+import DevImg from "./DevImg";
+import Socials from "./Socials";
+import Badge from "./Badge";
+const Hero = () => {
+  return (
+    <section className="py-12 xl:py-24 h-[84vh] xl:pt-28 bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none">
+      <div className="container mx-auto">
+        <div className="flex justify-between gap-x-8">
+          <div className="flex max-w-[600px] flex-col justify-center mx-auto xl:mx-0 text-center xl:text-left">
+            <div className="text-sm uppercase font-semibold mb-4 text-primary tracking-[4px]">
+              Software Engineer
+            </div>
+            <h1 className="h1 mb-4">Hello, my name is Rishabh Srivastava</h1>
+            <p className="subtitle max-w-[490px] mx-auto xl:mx-0 mb-4">
+              A software wizard, driven by a relentless curiosity and boundless
+              enthusiasm, crafting the extraordinary through the art of coding.
+            </p>
+            <div className="flex flex-col gap-y-3 md:flex-row gap-x-3 mx-auto xl:mx-0 mb-12">
+              <Link href="/contact">
+                <Button className="gap-x-2 items-center">
+                  Contact me <Send size={18} />
+                </Button>
+              </Link>
+              <Button variant="secondary" className="gap-x-2 items-center">
+                Download CV <Download size={18} />
+              </Button>
+            </div>
+          </div>
+          <div className="hidden xl:flex relative">Image</div>
+        </div>
+        <div className="hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce">
+          <RiArrowDownSLine className="text-3xl text-primary" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
